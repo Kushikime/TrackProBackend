ea433d5dd201a599fab6d8d4bf08c25affbdd164
Kushikime <demcsakerik@gmail.com>
2023-06-30 20:20:53 +0200

    INITIAL: added nestjs + nestjs passport jwt auth

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(
+    private appService: AppService,
+    private authService: AuthService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    return req.user;
+  }
+}
